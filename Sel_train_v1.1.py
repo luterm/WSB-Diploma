8a0ceb485f1e13a374dd640fb03b932ba2832de8
luterm <luter.m@icloud.com>
2024-03-27 16:31:21 +0100

    sign_in_button added

--- Sel_train_v1.1.py
+++ Sel_train_v1.1.py
@@ -10,19 +10,26 @@ driver = webdriver.Chrome(service=ChromeService(executable_path=ChromeDriverMana
 # Navigate to the url
 driver.get("https://google.com")
 
-# Wait for some time to ensure the page is fully loaded
-time.sleep(3)
+# Wait for 'n'sec to ensure the page is fully loaded
+time.sleep(1)
 
-# Find the rejection button
+# Find the rejection_button
 rejection_button = driver.find_element(By.ID, "W0wltc")
 
-# Wait for 3 seconds before clicking the button
-time.sleep(3)
+# Wait for 'n' sec, then click the rejection_button
+time.sleep(2)
 rejection_button.click()
 
-time.sleep(5)
+# wait for 'n'sec, then push sign_in_button
+time.sleep(1)
+sign_in_button = driver.find_element(By.CLASS_NAME, "gb_Kd")
+sign_in_button.click()
+
+
+time.sleep(10)
 # Close the driver
 driver.quit()
+
 
 #find example test cases to check login frame and password
 #find an example of tests for collecting data from google search and collect them in a database created
