5d85d7aae59ccb369fd228547cd32ede09a0f643
luterm <luter.m@icloud.com>
2024-03-28 18:23:07 +0100

    #description corrected and tasks at the bottom described

--- Sel_train_v1.1.py
+++ Sel_train_v1.1.py
@@ -5,43 +5,43 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 import time
 
-# Initialize Chrome driver instance
+    # Initialize Chrome driver instance
 driver = webdriver.Chrome(service=ChromeService(executable_path=ChromeDriverManager().install()))
 
-# Navigate to the url
+    # Navigate to the url
 driver.get("https://google.com")
 
-# Wait for 'n'sec to ensure the page is fully loaded
+    # Wait for 'n'sec to ensure the page is fully loaded
 time.sleep(1)
 
-# Find the rejection_button
+    # Find the rejection_button
 rejection_button = driver.find_element(By.ID, "W0wltc")
 
-# Wait for 'n' sec, then click the rejection_button
+    # Wait for 'n' sec, then click the rejection_button
 time.sleep(1)
 rejection_button.click()
 
-# wait for 'n'sec, then push sign_in_button
+    # wait for 'n'sec, then push sign_in_button
 time.sleep(1)
 sign_in_button = driver.find_element(By.CLASS_NAME, "gb_Kd")
 sign_in_button.click()
 
-# find and fill in login frame with the correct account name and password - protect them first (config file? or smthg)
-# the following two lines doesn't work as they should - check out if 'send_keys' is implemented correctly
+    # find and fill in login frame with the correct account name and password - protect them first (config file? or smthg)      
 time.sleep(1)
 username_txt = driver.find_element(By.NAME, "identifier")
 username_txt.send_keys("some.heavy.user@gmail.com")
 
-# locate the "next" button, then click it
+    # locate the "next" button, then click it
 time.sleep(1)
 sign_in_next = driver.find_element(By.ID, "identifierNext")
 sign_in_next.click()
 
 
 time.sleep(8)
-# Close the driver
+    
+    # Close the driver
 driver.quit()
 
-
-#find example test cases to check login frame and password
-#find an example of tests for collecting data from google search and collect them in a database created
+# find a method to secure username and password to be used in code ; config file???
+# find example test cases to check login frame and password
+# find an example of tests for collecting data from google search and collect them in a database created
