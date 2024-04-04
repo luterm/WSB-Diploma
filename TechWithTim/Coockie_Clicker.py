0d46cc4e3d11b621a1cfbbaa6464498224d50492
luterm <luter.m@icloud.com>
2024-04-04 18:15:35 +0200

    web game site, automate legitimate interess switches finding and turnong off

---
+++ TechWithTim/Coockie_Clicker.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+#from selenium.webdriver.chrome.service import Service #check if this is neccessary!
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+#service = Service(executable_path="chromedrivercostam")
+
+driver = webdriver.Chrome()
+
+driver.get("https://orteil.dashnet.org/cookieclicker/")
+    #add automated coockies rejection by finding all the switches for 'legitimate interess' and turn them off
+    
+breakpoint()
