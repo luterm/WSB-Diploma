1965602e48b15e82c445a6220d022c70c7d5dabd
luterm <luter.m@icloud.com>
2024-04-04 17:02:25 +0200

    WebDriverWait implemented, expected conditions imported

--- TechWithTim/TWT_v1.0.py
+++ TechWithTim/TWT_v1.0.py
@@ -1,18 +1,37 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-
-import time
 
 
 driver = webdriver.Chrome()
 driver.get("https://google.com")
-time.sleep(2)
+
+WebDriverWait(driver, 5).until(
+    EC.presence_of_element_located((By.ID, "W0wltc"))
+)
 
 reject_all_button = driver.find_element(By.ID, "W0wltc")
-time.sleep(1)
+#time.sleep(1) 
 
 reject_all_button.click()
-time.sleep(3)
+time.sleep(1)
 
+WebDriverWait(driver, 5).until(
+    EC.presence_of_element_located((By.CLASS_NAME, "gLFyf"))
+)
+
+input_element = driver.find_element(By.CLASS_NAME, "gLFyf")
+#input_element.clear() - usefull in case of a 'search textbox' preloaded with any text in it
+input_element.send_keys("glut ziarnisty" + Keys.ENTER)
+
+
+
+breakpoint()
+
+
+driver.quit()
 #now prepare a regular, automated search function in main google window
