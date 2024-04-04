2c209bdd3fff62882286a960013ff536edf28b3d
luterm <luter.m@icloud.com>
2024-04-04 13:22:11 +0200

    commit suggested by vs code; minor changes

--- TechWithTim/TWT_v1.0.py
+++ TechWithTim/TWT_v1.0.py
@@ -1,9 +1,18 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
 import time
 
+
 driver = webdriver.Chrome()
-
 driver.get("https://google.com")
+time.sleep(2)
 
+reject_all_button = driver.find_element(By.ID, "W0wltc")
+time.sleep(1)
+
+reject_all_button.click()
 time.sleep(3)
 
+#now prepare a regular, automated search function in main google window
