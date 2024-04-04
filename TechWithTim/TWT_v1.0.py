527319757d66ad7bac2e8eae85753db562527b60
luterm <luter.m@icloud.com>
2024-04-04 17:49:54 +0200

    how to create an array for multiple links includin g a certain text

--- TechWithTim/TWT_v1.0.py
+++ TechWithTim/TWT_v1.0.py
@@ -28,9 +28,22 @@ input_element = driver.find_element(By.CLASS_NAME, "gLFyf")
 #input_element.clear() - usefull in case of a 'search textbox' preloaded with any text in it
 input_element.send_keys("glut ziarnisty" + Keys.ENTER)
 
-#to search the link by an included text form the 'text search'
-link = driver.find_element(By.PARTIAL_LINK_TEXT, "glut") #check with a text which differs from the 'search phrase'
+    #to search the link by an included text form the 'text search'
+    #check with a text which differs from the 'search phrase'
+WebDriverWait(driver, 5).until(
+    EC.presence_of_all_elements_located((By.PARTIAL_LINK_TEXT, "glut"))
+)
+link = driver.find_element(By.PARTIAL_LINK_TEXT, "glut")
+    #to search an exact text; use LINK_TEXT, without PARTIAL alias before
+    #it addressess to the first link met expected text search condition
 link.click()
+
+    #to search for all the elements including a certain phrase use this:
+#links = driver.find_elements(By.PARTIAL_LINK_TEXT, "gluty") #it should return an array of results, how to make it?
+#[element1, element2, element3]
+
+
+
 
 breakpoint()
 
