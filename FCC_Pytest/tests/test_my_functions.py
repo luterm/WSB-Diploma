a4e28d54f12e856b73a0fe8e3923eb0b43c19f5d
luterm <luter.m@icloud.com>
2024-04-11 15:05:00 +0200

    division by zero error as pytest.raises(), imported pytest for that function

--- FCC_Pytest/tests/test_my_functions.py
+++ FCC_Pytest/tests/test_my_functions.py
@@ -1,4 +1,17 @@
 import source.my_functions as my_functions 
 
+#def test_add():
+#    pass
+
 def test_add():
-    pass 
+    result = my_functions.add(number_one=1, number_two=2)
+
+
+def test_divide():
+    result = my_functions.divide(10, 5)
+    assert result == 2
+
+
+def test_divide_by_zero():
+    with pytest.raises(ZeroDivisionError):
+        my_functions.divide(10,0)
