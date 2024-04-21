d7b97f8b8648c15fbc9f05c19e051a1d9fdd3c0f
luterm <luter.m@icloud.com>
2024-04-21 13:21:13 +0200

    AssertEqual error != none resolved by exchanging 'print' to 'return' in mathod 'introduce_yourself'

--- Create_a_human/source/Human_creation.py
+++ Create_a_human/source/Human_creation.py
@@ -7,29 +7,27 @@ class Human:
  # initialization method (CONSTRUCTOR)
     def __init__(self, name):
         print(f"A new man named {name} is being created!")
-# object attribute
+ # object attribute
         self.name = name
 
-# METHOD - a function inside the CLASS
+ # METHOD - a function inside the CLASS
     def introduce_yourself(self):
-        print(f"Hello, my name is {self.name}.")
+        return(f"Hello, my name is {self.name}.")
 
-    def introduce(self, Human):
-        print(f"Here is {Human.name}!")
+    def introduce(self, person):
+        print(f"Here is {person.name}!")
 
 # default syntax suggested, to be compared with the original underneath
 #class Child(Human):
  #   def introduce_yourself(self):
   #      return super().introduce_yourself() 
 
-class Chlid(Human):
+class Child(Human):
     def introduce_yourself(self):
         print(f"Hi, I'm {self.name}")
 
     def go_play(self):
-        print("What a great fun, mindblowing!")
-
-
+        print("What a great fun, fuckin' mindblowing!")
 
 # creating the OBJECT
 # recreating from the CLASS
@@ -38,20 +36,19 @@ class Chlid(Human):
 
 human_01 = Human("Adam")
 human_02 = Human("Eve")
-man_child_01 = Chlid("Miranda")
+man_child_01 = Child("Bimbo")
 
 print(human_01.species)
 print(human_02.species)
 print(human_01.name)
 print(human_02.name)
-
+print(human_01.introduce_yourself())
 # checking OBJECT TYPE: print(type(human01))
 # checking OBJECT's CONTENT: print(dir(human01))
 
 human_01.introduce_yourself()
 human_01.introduce(human_02)
 man_child_01.introduce_yourself()
-
 man_child_01.go_play()
 
 
