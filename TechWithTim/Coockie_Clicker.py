9166f5c88f059d665ba23dafda7b6006afc92b24
luterm <luter.m@icloud.com>
2024-05-06 19:30:28 +0200

    Coockie Clicker, problem with privacy options choice automation unresolved-after the attempt to automate choice manager dissapears,then language choice box appears; so automated language choice for enlgilsh inserted

--- TechWithTim/Coockie_Clicker.py
+++ TechWithTim/Coockie_Clicker.py
@@ -12,4 +12,20 @@ driver = webdriver.Chrome()
 driver.get("https://orteil.dashnet.org/cookieclicker/")
     #add automated coockies rejection by finding all the switches for 'legitimate interess' and turn them off
     #is there a method to automatically detect such switches on any website by the global element of that type characteristics?
+
+WebDriverWait(driver, 3).until(                                    
+    EC.presence_of_element_located((By.CLASS_NAME, "fc-button-label"))
+)                                                           
+
+manage_options_button = driver.find_element(By.CLASS_NAME, "fc-button-label").click()
+
+time.sleep(3)
+
+WebDriverWait(driver, 2).until(
+    EC.presence_of_element_located((By.ID, "langSelect-EN"))
+)
+lang_choice_but = driver.find_element(By.ID, "langSelect-EN").click()
+breakpoint()
+
+
 breakpoint()
