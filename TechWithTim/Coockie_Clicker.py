cc8382d9f9d1243cd9fda9483acbe006bc3f42a4
luterm <luter.m@icloud.com>
2024-05-06 20:28:59 +0200

    CookiesClicker stopped working after 46 and above line of code added; check elemnts web location correctness

--- TechWithTim/Coockie_Clicker.py
+++ TechWithTim/Coockie_Clicker.py
@@ -13,7 +13,7 @@ driver.get("https://orteil.dashnet.org/cookieclicker/")
     #add automated coockies rejection by finding all the switches for 'legitimate interess' and turn them off
     #is there a method to automatically detect such switches on any website by the global element of that type characteristics?
 
-WebDriverWait(driver, 5).until(                                    
+WebDriverWait(driver, 6).until(                                    
     EC.presence_of_element_located((By.CLASS_NAME, "fc-button-label"))
 )                                                           
 
@@ -21,29 +21,41 @@ manage_options_button = driver.find_element(By.CLASS_NAME, "fc-button-label").cl
 
 time.sleep(3)
 
-WebDriverWait(driver, 5).until(
+WebDriverWait(driver, 6).until(
     EC.presence_of_element_located((By.ID, "langSelect-EN"))
 )
 lang_choice_but = driver.find_element(By.ID, "langSelect-EN").click()
 
 
-
+#program data variables
 cookie_id = "bigCookie"
 cookies_id = "cookies"
+product_price_prefix = "productPrice"
+product_prefix = "product"
 
-
-WebDriverWait(driver, 5).until(
+WebDriverWait(driver, 6).until(
     EC.presence_of_element_located((By.ID, cookie_id ))
 )
 cookie = driver.find_element(By.ID,cookie_id)
 
 while True:
     cookie.click()
-    cookies_count = driver.find_element(By.ID, cookies_id).text
-    print(cookies_count)
+    cookies_count = driver.find_element(By.ID, cookies_id).text.split(" ")[0]
+    cookies_count = int(cookies_count.replace(",", ""))
+#    print(cookies_count)
 
 
 
+    for i in range(4):
+        product_price = driver.find_element(By.ID, product_price_prefix +str(i))
 
+        if not product_price.isdigit():
+            continue
+
+        product_price = int(product_price.replace(",", ""))
+
+        if cookies_count >= product_price:
+            product = driver.find_element(By.ID, product_prefix + str(i)).click()
+            break
 
 breakpoint()
