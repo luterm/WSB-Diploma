1e851fc1c8905b59338c1e85601b6805f35fbefd
luterm <luter.m@icloud.com>
2024-05-08 18:15:59 +0200

    test_LinearDemo refactored, location data for web elements updated, tests works

--- Unittest_Diploma/test_LinearDemo.py
+++ Unittest_Diploma/test_LinearDemo.py
@@ -3,6 +3,7 @@ import random
 import string
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 
 
 #Class Create
@@ -10,42 +11,49 @@ from selenium.webdriver.common.by import By
 class LinearDemo(unittest.TestCase):
 
     def setUp(self):
-        """Setup method to initiate the Chrome browser."""
+        
         self.driver = webdriver.Chrome()
         self.driver.maximize_window()
         self.driver.implicitly_wait(2)
 
+        
     def test_linear_flow_testing(self):
         # Navigate to desired web page and verify web page is displayed
         self.driver.get("https://jignect.tech/")
-        self.assertTrue("Software Testing Company" in self.driver.title)
-
+        self.assertTrue("Software and QA Testing Company| JigNect Technologies Pvt Ltd" in self.driver.title) # why it knows what the 'title' is if not defined before?
+        
 
         # Clicka on the contact us button
-        contact_us_button = self.driver.find_element(By.XPATH, "//ul[@id='menu-main-menu']//a[text(='Contact Us']")
-        contact_us_button.click()
+        contact_us_button = self.driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/div/div/nav/ul/li[8]/a").click() # changed incorrect XPATH to actual
+       
 
         # Verify desired page i displayed
         contact_us_header = self.driver.find_element(By.CSS_SELECTOR, "div[class='contact-title'] h2").text
+        
         self.assertEqual(contact_us_header, "Let's talk", "Contact Us page does not displayed")
 
         # Generate random string
-        full_name = "".join(random.choice(string.ascii_letters) for _ in range(8))
+        full_name = "".join(random.choice(string.ascii_letters) for _ in range(11))
 
         # Enter value in full name and company name, select checkbox and click on submit button
         self.driver.find_element(By.CSS_SELECTOR, "input[placeholder='Full name']").send_keys(full_name)
-        self.driver.find_element(By.CSS_SELECTOR, "input[placeholder='Company']").send_keys("TechTonic")
+        self.driver.find_element(By.CSS_SELECTOR, "input[placeholder='Company']").send_keys("Bulepa")
+        time.sleep(3)
+        
         self.driver.find_element(By.CSS_SELECTOR, "span[id='checkboxOne']").click()
+        #breakpoint()
+    
         self.driver.find_element(By.CSS_SELECTOR, "input[value='Submit']").click()
 
         #verify the validation message for mandatory fields
-        expected_validation_message = 'The field is required'
+        expected_validation_message = 'The field is required.'
 
-        textarea_actual_validation_message = self.driver.find_element(By.CSS_SELECTOR, "textarea[name*='textarea'] + span").text
         
-        email_actual_validation_message = self.driver.find_element(By.CSS_SELECTOR, "input [name*='email'] + span").text
-
-        phone_number_actual_validation_message = self.driver.find_element(By.CSS_SELECTOR, "input[placeholder='Phone number] + span").text
+        textarea_actual_validation_message = self.driver.find_element(By.CSS_SELECTOR, "#contact-new-form > div:nth-child(2) > div > p > span > span").text # Changed to actual
+        
+        email_actual_validation_message = self.driver.find_element(By.CSS_SELECTOR, "#contact-new-form > div:nth-child(3) > div:nth-child(2) > p > span > span").text # Changed to actual
+        
+        phone_number_actual_validation_message = self.driver.find_element(By.CSS_SELECTOR, "#contact-new-form > div:nth-child(4) > div:nth-child(2) > p > span > span").text # Changed to actual
 
 
 
