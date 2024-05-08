58a1c6245a9d5b3bcd04fcf75c065f9f9c9c59e8
luterm <luter.m@icloud.com>
2024-05-08 11:44:24 +0200

    new project 'LinearDemo' created, def test_linear_flow doesn work

--- TechWithTim/Coockie_Clicker.py
+++ TechWithTim/Coockie_Clicker.py
@@ -13,7 +13,7 @@ driver.get("https://orteil.dashnet.org/cookieclicker/")
     #add automated coockies rejection by finding all the switches for 'legitimate interess' and turn them off
     #is there a method to automatically detect such switches on any website by the global element of that type characteristics?
 
-WebDriverWait(driver, 6).until(                                    
+WebDriverWait(driver, 10).until(                                    
     EC.presence_of_element_located((By.CLASS_NAME, "fc-button-label"))
 )                                                           
 
@@ -21,7 +21,7 @@ manage_options_button = driver.find_element(By.CLASS_NAME, "fc-button-label").cl
 
 time.sleep(3)
 
-WebDriverWait(driver, 6).until(
+WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.ID, "langSelect-EN"))
 )
 lang_choice_but = driver.find_element(By.ID, "langSelect-EN").click()
@@ -33,7 +33,7 @@ cookies_id = "cookies"
 product_price_prefix = "productPrice"
 product_prefix = "product"
 
-WebDriverWait(driver, 6).until(
+WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.ID, cookie_id ))
 )
 cookie = driver.find_element(By.ID,cookie_id)
@@ -42,7 +42,7 @@ while True:
     cookie.click()
     cookies_count = driver.find_element(By.ID, cookies_id).text.split(" ")[0]
     cookies_count = int(cookies_count.replace(",", ""))
-#    print(cookies_count)
+    print(cookies_count)
 
     for i in range(4):
         product_price = driver.find_element(By.ID, product_price_prefix + str(i)).text.replace(",", "")
